63c01aaf05b5489d5b7e3d323ef6b7fcf87843a1
wazid33 <mdwazidhussain68@gmail.com>
2025-06-27 09:43:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app.listen(3000, () => console.log("https://render.com/docs/troubleshooting-deploys"));
