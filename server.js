b4f64cbf20b2d7b7cadf459b01367ba7ea7b1dbf
wazid33 <mdwazidhussain68@gmail.com>
2025-06-27 13:21:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.post("/ask", async (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+  console.log("Server running on https://wazid-658m.onrender.com");
 });
